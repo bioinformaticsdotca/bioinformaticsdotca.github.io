588001635ccd107380d66ef3750d6e058a330d43
Canadian Bioinformatics Workshops <support@bioinformatics.ca>
2025-05-20 12:21:21 -0600

    Revert "H5P Integration plugin testing"

--- h5p-standalone-3.8.0/dist/h5p-integration.d.ts
+++
@@ -1,2 +0,0 @@
-import { H5PIntegration } from './h5p';
-export declare function defaultH5PIntegration(): H5PIntegration;
--- h5p-standalone-3.8.0/dist/utils.d.ts
+++
@@ -1,6 +0,0 @@
-export declare function urlPath(path: string): string;
-export declare function getJSON<T>(url: string, requestOptions?: RequestInit): Promise<T>;
-export declare function loadScripts(target: HTMLElement, scripts: string[]): Promise<void>;
-export declare function loadStylesheets(target: HTMLElement, styles: string[]): void;
-export declare function mergeObject<T>(target: any, ...sources: any): T;
-export declare function mergeArrayUnique(array1: string[], array2: string[]): string[];
